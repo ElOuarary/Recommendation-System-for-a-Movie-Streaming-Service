7542f303d7a5b701eb9de0a5fdde2583eba3c582
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-11-14 13:54:35 +0100

    Make some changes on the script for filtering data making it possible to filter data between specif ranges

--- scripts/filtering.py
+++ scripts/filtering.py
@@ -3,6 +3,7 @@ from data_loading import dataframes
 # Import the necessary modules
 import logging
 import pandas as pd
+import numpy as np
 
 """Need to fix this logging arguments values due to a collision with data_processing.log"""
 # Set up logging configuration
@@ -17,18 +18,24 @@ logger = logging.getLogger()
 
 
 # Create a filert by rating function that filter the movies based on their rating
-def filter_by_rating(rating: float, inplace = False):
+def filter_by_rating(min_rating: float, max_rating: float = 5, inplace = False):
     """Filter the rating csv file based on the rating
     
-    rating:
-    - Define the rating on which the filter will start up to 5 maximun
+    min_rating:
+    - Define the rating on which the filter will start up [min_rating, 5]
+
+    max_rating:
+    - Define the rating on which the filter will stop [min_rating, max_rating] to default value is 5
 
     inplace:
     -boolean value that either return a copy of the filtred DataFrame or not, default value is set to False
     """
-    df = dataframes["ratings"] if inplace else dataframes["ratings"].copy()
-    df = df[df["rating"] >= rating]
-    logger.debug(f"The rating.csv file was filtred")
-    return df.to_csv("data/processed/filtered_ratings.csv")
+    try:
+        df = dataframes["ratings"] if inplace else dataframes["ratings"].copy()
+        df = df[np.logical_and(df["rating"] >= min_rating, df["rating"] <= max_rating)]
+        logger.debug(f"The rating.csv file was filtred")
+        return df.to_csv("data/processed/filtered_ratings.csv")
+    except ValueError as e:
+        logger.error(f"{e}: the value of one of the parameter caused error.")
 
-filter_by_rating(5)
+filter_by_rating(3, 5)
