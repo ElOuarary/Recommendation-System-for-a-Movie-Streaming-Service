27e146b284c139f6ce55fdbf795b1e28ba4ff5f3
El Ouarary Abdelaziz <122648837+ElOuarary@users.noreply.github.com>
2024-11-26 14:25:50 +0100

    Fixed the user matrix filling values based on the cordinates of both user Id and movie Id

--- scripts/data_transforming.py
+++ scripts/data_transforming.py
@@ -37,17 +37,18 @@ try:
 
     # Initialize a sparse matrix and fill it
     user_item_matrix = lil_matrix((num_users, num_movies))
-    for user_idx, movie_idx, rating in zip(user_mapping, movie_mapping, df["rating"]):
+    for user_idx, movie_idx, rating in zip(user_mapping, df["movieId"] - 1, df["rating"]):
         user_item_matrix[user_idx, movie_idx] = rating
 
+    
     # Normalization the output
     user_item_dense = user_item_matrix.toarray()
     row_means = np.mean(user_item_dense, axis=1, keepdims=True, where=(user_item_dense!=0))
-    normalized_user_item = user_item_dense - row_means
-    
+    normalized_user_item = np.subtract(user_item_dense, row_means, where=(user_item_dense!=0))
+        
     # Load the output to a csv file if "data/processed"
-    pd.DataFrame(normalized_user_item).to_csv("data/processed/output.csv")
-    logger.info(f"Normalized matrix saved to data/processed/output.csv")
+    #pd.DataFrame(normalized_user_item).to_csv("data/processed/output.csv")
+    #logger.info(f"Normalized matrix saved to data/processed/output.csv")
 
 except Exception as e:
     logger.error(f"Error during data transformation: {e}")
